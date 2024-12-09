1410f8ead53fe3a34cc6fe7558e82097d160f7d7
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-09 21:24:23 +0200

    create splash

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
+import 'package:fruit_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruit_hub/core/utils/app_images.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.imagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(
+          Assets.imagesSplashBottom,
+          fit: BoxFit.fill,
+        ),
+      ],
+    );
+  }
+}
