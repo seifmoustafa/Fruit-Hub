6ca3cd4e83171bb551982d1fdf82a7f66f671e95
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-10 20:57:25 +0200

    `Added DotsIndicator package and updated OnBoardingViewBody widget to include dots indicator`

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xff1B5E37);
+  // static const Color secondaryColor = Color(0xff000000);
+  // static const Color backgroundColor = Color(0xffF5F5F5);
+}
--- lib/features/on_boarding/presentation/views/widgets/on_boardign_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boardign_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -6,8 +8,19 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [Expanded(child: OnBoardingPageView())],
+    return Column(
+      children: [
+        const Expanded(
+          child: OnBoardingPageView(),
+        ),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color: AppColors.primaryColor.withOpacity(0.5),
+          ),
+        ),
+      ],
     );
   }
 }
