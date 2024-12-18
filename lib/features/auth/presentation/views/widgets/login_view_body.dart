b642c337c54bc7db608cc0cf92da8eef7fe929b3
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-18 22:19:17 +0200

    `Added SignupView to onGenerateRoute and updated LoginViewBody`

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/constants.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_images.dart';
 import 'package:fruit_hub/core/utils/app_text_styles.dart';
