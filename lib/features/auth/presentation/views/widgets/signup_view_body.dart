7bf8d42e478bd9c1926e4924af29f5e813617eb2
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-18 22:30:52 +0200

    `Added form fields to SignupViewBody widget`

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/constants.dart';
+import 'package:fruit_hub/core/widgets/custom_text_form_field.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+        child: Column(
+          spacing: 16,
+          children: [
+            CustomTextFormField(
+                hintText: 'الاسم كامل', textInputType: TextInputType.name),
+            CustomTextFormField(
+                hintText: 'البريد الإلكتروني',
+                textInputType: TextInputType.emailAddress),
+            CustomTextFormField(
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffc9cecf),
+                ),
+                hintText: 'كلمة المرور',
+                textInputType: TextInputType.visiblePassword)
+          ],
+        ),
+      ),
+    );
   }
 }
