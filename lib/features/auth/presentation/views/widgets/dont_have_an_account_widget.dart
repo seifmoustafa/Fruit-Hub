0cd59303294987634a6b6546a600982b2702c03a
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-18 22:21:38 +0200

    `Added import for gestures and TapGestureRecognizer, and implemented navigation to SignupView on tap.`

--- lib/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart
+++ lib/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_text_styles.dart';
+import 'package:fruit_hub/features/auth/presentation/views/signup_view.dart';
 
 class DontHaveAnAccountWidget extends StatelessWidget {
   const DontHaveAnAccountWidget({
@@ -22,6 +24,10 @@ class DontHaveAnAccountWidget extends StatelessWidget {
               style: TextStyles.semiBold16
                   .copyWith(color: const Color(0xFF616A6B))),
           TextSpan(
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                Navigator.pushNamed(context, SignupView.routeName);
+              },
             text: 'قم بإنشاء حساب',
             style:
                 TextStyles.semiBold16.copyWith(color: AppColors.primaryColor),
