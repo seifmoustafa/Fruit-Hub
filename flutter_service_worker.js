72ec212d0f2716aa055c48cf35037d97717ace9b
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-20 17:14:19 +0200

    Include build/web directory for deployment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -50,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e942404696e955b3a3e4db0bb005ff4c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "2b53eeeb581362a461ec5bd5b520503e",
+"flutter_bootstrap.js": "7a96a7f7066df00226577d99c95d042b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
