d5650611e7abe7dad3848806eda259eb8608c115
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-25 11:33:09 +0200

    Integrate SigninCubit into SigninView and update widget structure for BlocProvider

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruit_hub/core/services/get_it_service.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:fruit_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+import 'package:fruit_hub/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
 
 class SigninView extends StatelessWidget {
   const SigninView({super.key});
   static const routeName = '/login';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'تسجيل الدخول'),
-      body: const SigninViewBody(),
+    return BlocProvider(
+      create: (context) => SigninCubit(getIt.get<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'تسجيل الدخول'),
+        body: const SigninViewBody(),
+      ),
     );
   }
 }
