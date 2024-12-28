0d720296fa9afb3ca77fb97f93e3b177311a77dc
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-28 23:01:54 +0200

    Include build/web directory for deployment

--- lib/constants.dart
+++
@@ -1,2 +0,0 @@
-const kHorizontalPadding = 16.5;
-const kIsOnBoardingViewSeen = 'isOnBoardingViewSeen';
--- lib/core/errors/faliure.dart
+++
@@ -1,9 +0,0 @@
-abstract class Failure {
-  final String message;
-
-  Failure({required this.message});
-}
-class ServerFaliure extends Failure {
-  ServerFaliure({required super.message});
-  
-}
