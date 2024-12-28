1577746204b3d88c2a5d82eacf1241a1f3b932a1
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-12-28 23:12:15 +0200

    Update web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -59,7 +59,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ff0c03766e637cb445239e1bcae5880b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "5df32fe3fbb675d7251cd91d51fe5f19",
+"flutter_bootstrap.js": "663b6eb0169d919e21e6b954c5ae1123",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
